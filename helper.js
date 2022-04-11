7331ca397637e5ddf19f700e3afd82f75820a5ff
Ítalo Vinícius <italovinicius18@yahoo.com>
2022-04-11 09:31:14 -0300

    Criando requisiçao de cadastro
    Co-authored-by: Álvaro Leles Guimarães <alvaro.leles@hotmail.com>

---
+++ helper.js
@@ -0,0 +1,84 @@
+async function getIdLocalizacao(sql, cidade, uf) {
+  let idLocalizacao;
+  const queryResgataLocalizacao =
+    await sql.query`SELECT idLocalizacao FROM LOCALIZACAO WHERE cidade = ${cidade} AND uf=${uf}`;
+
+  if (queryResgataLocalizacao.rowsAffected[0] == 0) {
+    // Se localização não existir, insere e já resgata seu id
+    const queryInserirLocalizacao =
+      await sql.query`INSERT INTO LOCALIZACAO(cidade,uf) VALUES (${cidade},${uf})`;
+    const queryResgataLocalizacao =
+      await sql.query`SELECT idLocalizacao FROM LOCALIZACAO WHERE cidade = ${cidade} AND uf=${uf}`;
+    idLocalizacao = queryResgataLocalizacao.recordset[0].idLocalizacao;
+  } else {
+    idLocalizacao = queryResgataLocalizacao.recordset[0].idLocalizacao;
+  }
+
+  return idLocalizacao;
+}
+
+async function getIdInstituicao(sql, nomeInstituicao, idEscolaridade) {
+  let idInstituicao
+  const queryResgataLocalizacao =
+    await sql.query`SELECT idInstituicao FROM INSTITUICAO WHERE nomeInstituicao = ${nomeInstituicao}`;
+
+  if (queryResgataLocalizacao.rowsAffected[0] == 0) {
+    // Se localização não existir, insere e já resgata seu id
+    const queryInserirLocalizacao =
+      await sql.query`INSERT INTO INSTITUICAO(nomeInstituicao, idEscolaridade) VALUES (${nomeInstituicao},${idEscolaridade})`;
+    const queryResgataLocalizacao =
+      await await sql.query`SELECT idInstituicao FROM INSTITUICAO WHERE nomeInstituicao = ${nomeInstituicao}`;
+    idInstituicao = queryResgataLocalizacao.recordset[0].idInstituicao;
+  } else {
+    idInstituicao = queryResgataLocalizacao.recordset[0].idInstituicao;
+  }
+
+  return idInstituicao;
+}
+
+async function getIdCurso(sql, nomeCurso, idEscolaridade) {
+  let idCurso;
+  const queryResgataLocalizacao =
+    await sql.query`SELECT idCurso FROM CURSO WHERE nomeCurso = ${nomeCurso}`;
+
+  if (queryResgataLocalizacao.rowsAffected[0] == 0) {
+    // Se localização não existir, insere e já resgata seu id
+    const queryInserirLocalizacao =
+      await sql.query`INSERT INTO CURSO(nomeCurso,idEscolaridade) VALUES (${nomeCurso},${idEscolaridade})`;
+    const queryResgataLocalizacao =
+      await sql.query`SELECT idCurso FROM CURSO WHERE nomeCurso = ${nomeCurso}`;
+    idCurso = queryResgataLocalizacao.recordset[0].idCurso;
+  } else {
+    idCurso = queryResgataLocalizacao.recordset[0].idCurso;
+  }
+
+  return idCurso;
+}
+
+async function verificaCadidatoExiste(sql, cpf) {
+  const queryCandidato = await sql.query`SELECT cpf FROM CANDIDATO WHERE cpf=${cpf}`;
+  console.log(queryCandidato)
+  if (queryCandidato.rowsAffected[0] == 0){
+    return false
+  }
+  
+  return true
+}
+
+async function verificaEmpresaExiste(sql, cnpj) {
+  const queryEmpresa = await sql.query`SELECT cnpj FROM EMPRESA WHERE cnpj=${cnpj}`;
+  console.log(queryEmpresa)
+  if (queryEmpresa.rowsAffected[0] == 0){
+    return false
+  }
+  
+  return true
+}
+
+module.exports = {
+  getIdLocalizacao,
+  getIdInstituicao,
+  getIdCurso,
+  verificaCadidatoExiste,
+  verificaEmpresaExiste
+};
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const helper = require("./helper");
+
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -29,7 +31,7 @@ sql
     console.log("Conectando ao banco");
   })
   .then((result) => {
-    console.log("Conectado");
+    console.log("Conectado..");
   })
   .catch((err) => {
     console.log("Erro ao se conectar com o banco de dados");
@@ -43,6 +45,94 @@ app.get("/teste", async (req, res) => {
   const response = await sql.query`SELECT * from teste`;
   console.log(response);
   res.send(response.recordset);
+});
+
+app.post("/cadastro", async (req, res) => {
+  const corpo = req.body;
+  const tipoUsuario = corpo.tipoUsuario;
+
+  if (tipoUsuario === "candidato") {
+    const nome = corpo.nome;
+    const cpf = corpo.cpf;
+    const senha = corpo.senha;
+    const cidade = corpo.cidade;
+    const uf = corpo.uf;
+    const idEscolaridade = corpo.idEscolaridade;
+    const curriculo = corpo.curriculo;
+    const areasInteresse = corpo.areasInteresse;
+    const descricaoCandidato = corpo.descricaoCandidato;
+
+    if (corpo.email !== undefined) {
+      var email = corpo.email
+    }
+    else{
+      var email = ''
+    }
+
+    if (corpo.nomeInstituicao !== undefined) {
+      const nomeInstituicao = corpo.nomeInstituicao;
+      var idInstituicao = await helper.getIdInstituicao(
+        sql,
+        nomeInstituicao,
+        idEscolaridade
+      );
+    } else {
+      var idInstituicao = "";
+    }
+
+    if (corpo.nomeCurso !== undefined) {
+      const nomeCurso = corpo.nomeCurso;
+      var idCurso = await helper.getIdCurso(sql, nomeCurso, idEscolaridade);
+    } else {
+      var idCurso = "";
+    }
+
+    let idLocalizacao = await helper.getIdLocalizacao(sql, cidade, uf);
+
+    if (await helper.verificaCadidatoExiste(sql, cpf)) {
+      console.log("Candidato já existe");
+      res.status(500).send("Candidato já existe");
+    } else {
+      console.log("Cadastra candidato");
+      const queryInsereCandidato =
+        await sql.query`INSERT INTO 
+        CANDIDATO(cpf,senha,nomeCompleto,email,descricaoCandidato,curriculo,idEscolaridade,idInstituicao,idCurso,idLocalizacao,areasInteresse) 
+           VALUES(${cpf},${senha},${nome},${email},${descricaoCandidato},NULL,${idEscolaridade},${idInstituicao},${idCurso},${idLocalizacao},${areasInteresse})`;
+      
+      res.send("Cadastra candidato");
+    }
+  } else if (tipoUsuario === "empresa") {
+    if (await helper.verificaEmpresaExiste(sql, corpo.cnpj))
+      res.status(500).send("Empresa já cadastrada no sistema");
+    else
+    {
+      let idLocalizacao = await helper.getIdLocalizacao(sql, corpo.cidade, corpo.uf);
+  
+      await sql.query `INSERT INTO EMPRESA
+                      VALUES(${corpo.cnpj}, ${corpo.senha}, ${corpo.nomeEmpresa}, ${corpo.siteEmpresa}, ${corpo.descricaoEmpresa}, ${idLocalizacao})`;
+  
+      res.send("Empresa cadastrada");
+    }
+  }
+});
+
+app.get("/login", async (req, res) => {
+  const corpo = req.body;
+  const credencial = corpo.credencial;
+  const senha = corpo.senha;
+
+  if (credencial.length == 14) {
+    console.log("CNPJ recebido");
+
+    const query =
+      await sql.query`SELECT * from EMPRESA WHERE cnpj=${credencial} AND senha=${senha}`;
+
+    console.log(query);
+  } else if (credencial.length == 11) {
+    console.log("CPF recebido");
+  } else {
+    res.status(500).send("Algo deu errado!");
+  }
 });
 
 app.listen(PORT, HOST, () => console.log(`Listening on port ${PORT}`)); //Informa a porta por qual o serviço está "ouvindo"
